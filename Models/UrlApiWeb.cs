bf239230465c59cccbfcbaa51046089e7d3e83ff
Fúlvio <fulviocanducci@hotmail.com>
2018-05-10 10:54:45 -0300

    Update Register

---
+++ Models/UrlApiWeb.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public static class UrlApiWeb
+    {
+        public static string BaseUrl = "http://localhost:8181/api/";
+
+        public static string GetBaseUrl(string url)
+        {
+            return $"{BaseUrl}{url}";
+        }
+    }
+}
